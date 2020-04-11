cf0968e082e883c6647ae71c7919246710013849
takumi satake <tako_39@docomo.ne.jp>
2020-04-11 12:53:48 +0900

    シーンのロード

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+    [SerializeField]
+    private float fadeInTime;
+
+    private Image image;
+
+    private void Start()
+    {
+        image = transform.Find("Panel").GetComponent<Image>();
+        fadeInTime = 1f * fadeInTime / 10f;
+        StartCoroutine("FadeInFunc");
+    }
+
+    IEnumerator FadeInFunc()
+    {
+        for(var i = 1f; i >= 0f; i -= 0.1f)
+        {
+            image.color = new Color(0f, 0f, 0f, i);
+            yield return new WaitForSeconds(fadeInTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    private AsyncOperation async;
+
+    [SerializeField]
+    private GameObject loadUI;
+
+    [SerializeField]
+    private Slider slider;
+
+    public void NextScene()
+    {
+        loadUI.SetActive(true);
+
+        StartCoroutine("LoadData");
+    }
+
+    IEnumerator LoadData()
+    {
+        async = SceneManager.LoadSceneAsync("Load1");
+
+        while (!async.isDone)
+        {
+            var progressVal = Mathf.Clamp01(async.progress / 0.9f);
+            slider.value = progressVal;
+            yield return null;
+        }
+    }
+}
